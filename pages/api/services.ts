a1b0bcd5161d23720515e3e8ae9cb1678ec2c036
Mohammad Suleman <msz365@hotmail.com>
2022-11-30 15:58:40 +0500

    updated nextjs

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { services } from "../../data";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default  (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ services });
 };
